2ad05b9220e7c34d0e73177e877e0b2c01a920f7
Marcello López <mlc74163322@gmail.com>
2024-01-24 00:44:32 -0300

    Cambios con sonidos

--- src/components/Sounds/SoundContext.jsx
+++ src/components/Sounds/SoundContext.jsx
@@ -1,21 +1,54 @@
-import React, { createContext, useContext, useState } from 'react';
+// src/components/Sounds/Sounds.jsx
+import React, { createContext, useContext, useState, useCallback, useEffect } from 'react';
 
 const SoundContext = createContext();
 
 export const useSound = () => {
-  return useContext(SoundContext);
+  const context = useContext(SoundContext);
+  if (!context) {
+    throw new Error('useSound debe ser utilizado dentro de un SoundProvider');
+  }
+  return context;
 };
 
-export const SoundProvider = ({ children }) => {
-  const [allowSounds, setAllowSounds] = useState(false);
+const Sounds = ({ children }) => {
+  const [allowSounds, setAllowSounds] = useState(true);
 
-  const toggleSounds = (value) => {
-    setAllowSounds(value);
+  const toggleSounds = useCallback(() => {
+    setAllowSounds((prevAllowSounds) => !prevAllowSounds);
+  }, []);
+
+  const playSound = useCallback((audioFile, volume) => {
+    if (allowSounds) {
+      const audioElement = new Audio(audioFile);
+      audioElement.volume = volume;
+      audioElement.play();
+    }
+  }, [allowSounds]);
+
+  const stopMusic = useCallback(() => {
+    // Puedes agregar lógica para detener la música si es necesario
+  }, []);
+
+  // Lista de archivos de sonido que quieres precargar
+  const soundFiles = ['/src/assets/fx/pickUpCoin.wav', '/src/assets/fx/bg-music.mp3', '/src/assets/fx/progress.mp3', '/src/assets/fx/enter.mp3'];
+
+  // Precarga los archivos de sonido cuando el componente se monta
+  useEffect(() => {
+    soundFiles.forEach((audioFile) => {
+      const audioElement = new Audio(audioFile);
+      audioElement.preload = 'auto';
+    });
+  }, []);
+
+  const value = {
+    allowSounds,
+    toggleSounds,
+    playSound,
+    stopMusic,
   };
 
-  return (
-    <SoundContext.Provider value={{ allowSounds, toggleSounds }}>
-      {children}
-    </SoundContext.Provider>
-  );
+  return <SoundContext.Provider value={value}>{children}</SoundContext.Provider>;
 };
+
+export default Sounds;
